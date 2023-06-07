f88d9f06cf4e06adc462a46285d659d33de46bec
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2023-06-07 10:19:08 +0200

    fixed sessions

--- src/account.php
+++ src/account.php
@@ -1,6 +1,8 @@
 <?php
 require_once("../includes/connection.php");
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+	session_start();
+}
 ?>
 <?php
 if (isset($_SESSION['UserID'])) {
