6ad926341c360b8b18f3602e1644dac1770cb401
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2023-06-07 16:24:28 +0200

    working cart big checkpoint

--- src/cart.php
+++ src/cart.php
@@ -70,11 +70,12 @@ if (!isset($_SESSION['UserID'])) {
 						<th>Item</th>
 						<th>Price</th>
 						<th>Quantity</th>
+						<th>Remove</th>
 					</tr>
 					<?php
 					// Retrieve cart items for the logged-in user
 					$userID = $_SESSION['UserID'];
-					$sql = "SELECT items.Name, items.SalesPrice, user_cart.Quantity
+					$sql = "SELECT items.ItemID, items.Name, items.SalesPrice, user_cart.Quantity
 							FROM user_cart
 							INNER JOIN items ON user_cart.ItemID = items.ItemID
 							WHERE user_cart.UserID = $userID";
@@ -88,6 +89,7 @@ if (!isset($_SESSION['UserID'])) {
 
 						// Iterate over the cart items and display them
 						while ($row = mysqli_fetch_assoc($result)) {
+							$itemID = $row['ItemID'];
 							$itemName = $row['Name'];
 							$salesPrice = $row['SalesPrice'];
 							$quantity = $row['Quantity'];
@@ -104,6 +106,9 @@ if (!isset($_SESSION['UserID'])) {
 							echo "</td>";
 							echo "<td class='price-cell'>R" . number_format($salesPrice, 2) . "</td>";
 							echo "<td class='quantity-cell'>$quantity</td>";
+							echo "<td class='remove-cell'>";
+							echo "<button class='remove-button' onclick='removeItem($itemID)'>Remove</button>";
+							echo "</td>";
 							echo "</tr>";
 						}
 
@@ -115,7 +120,9 @@ if (!isset($_SESSION['UserID'])) {
 						$shipping = ($subtotal >= 1000) ? 0 : 100;
 					} else {
 						// Handle the case when the cart is empty
-						echo "<tr><td colspan='3'>Your cart is empty.</td></tr>";
+						$subtotal = 0;
+						$vat = 0;
+						$shipping = 0;
 					}
 
 					// Close the database connection
@@ -127,27 +134,61 @@ if (!isset($_SESSION['UserID'])) {
 
 		<div class="order-summary">
 			<table>
-				<tr>
-					<td>Subtotal:</td>
-					<td>R<?php echo number_format($subtotal * 0.85, 2); ?></td>
-				</tr>
-				<tr>
-					<td>VAT (15%):</td>
-					<td>R<?php echo number_format($vat, 2); ?></td>
-				</tr>
-				<tr>
-					<td>Shipping:</td>
-					<td>R<?php echo number_format($shipping, 2); ?></td>
-				</tr>
-				<tr class="total-row">
-					<td>Total:</td>
-					<td>R<?php echo number_format($total, 2); ?></td>
-				</tr>
+				<?php if ($subtotal > 0) { ?>
+					<tr>
+						<td>Subtotal:</td>
+						<td>R<?php echo number_format($subtotal * 0.85, 2); ?></td>
+					</tr>
+					<tr>
+						<td>VAT (15%):</td>
+						<td>R<?php echo number_format($vat, 2); ?></td>
+					</tr>
+					<tr>
+						<td>Shipping:</td>
+						<td>R<?php echo number_format($shipping, 2); ?></td>
+					</tr>
+					<tr class="total-row">
+						<td>Total:</td>
+						<td>R<?php echo number_format($total, 2); ?></td>
+					</tr>
+				<?php } else { ?>
+					<tr>
+						<td colspan="2">Your cart is empty.</td>
+					</tr>
+				<?php } ?>
 			</table>
+		</div>
+
+
+		<div class="checkout-button">
+			<button onclick="checkout()">Checkout</button>
 		</div>
 	</div>
 
 	<?php include_once "../includes/footer.php"; ?>
+
+	<script>
+		// Function to remove an item from the cart
+		function removeItem(itemID) {
+			// Send an AJAX request to the remove_item.php script
+			var xhr = new XMLHttpRequest();
+			xhr.open("POST", "/includes/remove-item.php", true);
+			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState === 4 && xhr.status === 200) {
+					// Reload the page to update the cart
+					location.reload();
+				}
+			};
+			xhr.send("itemID=" + itemID);
+		}
+
+		// Function to handle the checkout process
+		function checkout() {
+			// Implement your checkout logic here
+			alert("Thank you for your purchase!");
+		}
+	</script>
 </body>
 
 </html>
