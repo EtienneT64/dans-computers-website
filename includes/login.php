7e6fa5dd1c62515bdd71b1476fed22b8a46fc488
Etienne_T <110019770+EtienneT64@users.noreply.github.com>
2023-09-25 10:47:41 +0200

    Update login.php

--- includes/login.php
+++ includes/login.php
@@ -69,6 +69,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+	<link rel="icon" href="/images/favicon.png" type="image/png">
+
 	<link rel="stylesheet" type="text/css" href="/styles/account.css" />
 	<link rel="stylesheet" type="text/css" href="/styles/login.css" />
 
