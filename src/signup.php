bff8d6fc808bf5ac36b50db4782eed693e49ae5d
Etienne_T <110019770+EtienneT64@users.noreply.github.com>
2023-09-25 10:47:09 +0200

    Update signup.php

--- src/signup.php
+++ src/signup.php
@@ -68,6 +68,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+	<link rel="icon" href="/images/favicon.png" type="image/png">
+
 	<link rel="stylesheet" type="text/css" href="/styles/signup.css" />
 
 	<script defer src="../scripts/signup.js"></script>
